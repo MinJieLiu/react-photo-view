0bc2ac5221575ed44d29fbd3d0235325505d0f4d
liumingyi_1 <liumingyi_1@163.com>
2019-11-20 14:00:08 +0800

    小优化

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -43,7 +43,7 @@ class App extends React.Component {
           <ImageList>
             {photoImages.map((item, index) => (
               <PhotoConsumer key={index} src={item} intro={item}>
-                <SmallImage />
+                {index < 2 ? <SmallImage src={item} /> : undefined}
               </PhotoConsumer>
             ))}
           </ImageList>
